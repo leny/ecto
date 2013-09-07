a74172c7f0a93fe9d667782c7a0914bb1744c3b5
Leny <info@flatland.be>
2013-09-07 18:50:59 +0200

    Display one post

--- server/controllers/public.js
+++ server/controllers/public.js
@@ -12,18 +12,21 @@
 
 var root = __dirname + "/..",
     fs = require( "fs" ),
-    pkg = require( root + "/../package.json" );
+    markdown = require( "markdown" ).markdown,
+    pkg = require( root + "/../package.json" ),
+    sPostsPath = root + "/../" + pkg.config.posts;
 
 var homepage = function( oRequest, oResponse ) {
-    var sPostsPath = root + "/../" + pkg.config.posts,
-        aPostsFiles = fs.readdirSync( sPostsPath ),
+    var aPostsFiles = fs.readdirSync( sPostsPath ),
         aPosts = [],
         iNow = ( new Date() ).getTime(),
-        i, sPostFile, oPost, iPostDate;
+        i, sPostFile, oPost, dPostDate, iPostDate;
     for( i = -1; sPostFile = aPostsFiles[ ++i ]; ) {
         oPost = require( sPostsPath + sPostFile );
-        iPostDate = ( new Date( oPost.date ) ).getTime();
+        iPostDate = ( dPostDate = new Date( oPost.date ) ).getTime();
         if( iPostDate <= iNow ) {
+            oPost.url = sPostFile.replace( ".json", "" ) + "-" + oPost.title.toLowerCase().replace( /[^a-z0-9]+/g, "-" ) + ".html";
+            oPost.date = dPostDate.toUTCString();
             aPosts.push( oPost );
         }
     }
@@ -33,6 +36,27 @@ var homepage = function( oRequest, oResponse ) {
     } );
 }; // homepage
 
+var article = function( oRequest, oResponse ) {
+    var sPostFile = sPostsPath + oRequest.params.date + ".json",
+        iNow = ( new Date() ).getTime(),
+        oPost, dPostDate, iPostDate;
+    if( !fs.existsSync( sPostFile ) ) {
+        return oResponse.send( 404 );
+    }
+    oPost = require( sPostFile );
+    iPostDate = ( dPostDate = new Date( oPost.date ) ).getTime();
+    if( iPostDate > iNow ) {
+        return oResponse.send( 403 );
+    }
+    oPost.date = dPostDate.toUTCString();
+    oPost.content = markdown.toHTML( oPost.content );
+    oResponse.render( "public/article", {
+        "pageTitle": "ecto > " + oPost.title,
+        "article": oPost
+    } );
+}; // article
+
 exports.init = function( oApp ) {
     oApp.get( "/", homepage );
+    oApp.get( "/:date-:name.html", article );
 };
