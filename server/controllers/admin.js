0e04ea456a993a88fc1d9f33ca7cd4e7582afc74
Leny <info@flatland.be>
2013-09-07 23:35:30 +0200

    Save new post

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -72,13 +72,52 @@ var addPost = function( oRequest, oResponse ) {
     } );
 }; // addPost
 
+var savePost = function( oRequest, oResponse ) {
+    var _month, _day, _hours, _minutes, oPostObject, sFileName, sFilePath,
+        dNow = new Date(),
+        iNowYear = dNow.getFullYear(),
+        sNowMonth = ( _month = dNow.getMonth() ) < 9 ? ++_month : "0" + ( ++_month ),
+        sNowDay = ( _day = dNow.getDate() ) < 10 ? _day : "0" + _day,
+        sNowHours = ( _hours = ( dNow.getHours() ) ) < 10 ? ++_hours : "0" + ( ++_hours ),
+        sNowMinutes = ( _minutes = ( dNow.getMinutes() ) ) < 10 ? ++_minutes : "0" + ( ++_minutes ),
+        sNowSeconds = "00";
+    oPostObject = {
+        "title": oRequest.body.title || "Untitled post",
+        "date": ( oRequest.body.date || iNowYear + "-" + sNowMonth + "-" + sNowDay ) + " " + ( oRequest.body.time || sNowHours + ":" + sNowMinutes + ":" + sNowSeconds ),
+        "content": oRequest.body.content
+    };
+    sFileName = oPostObject.date.replace( /([\s:-]?)/g, "" ) + ".json";
+    sFilePath = sPostsPath + sFileName;
+    if( !!oRequest.body.file ) {
+        // EDIT
+        // TODO check file existence
+        // TODO write in file
+        // TODO rename file if needed
+    }
+    fs.writeFile( sFilePath, JSON.stringify( oPostObject ), function( oError ) {
+        if( oError ) {
+            return oResponse.render( "admin/edit", {
+                "pageTitle": "ajouter un billet",
+                "post": {
+                    "title": oRequest.body.title,
+                    "date": oRequest.body.date,
+                    "time": oRequest.body.time,
+                    "content": oRequest.body.content
+                },
+                "error": true
+            } );
+        }
+        oResponse.redirect( "/admin/list" );
+    } );
+}; // savePost
+
 exports.init = function( oApp ) {
     oApp.get( "/admin", connexion );
     oApp.post( "/admin", login );
     oApp.get( "/admin/list", adminMiddleware, listPosts );
     oApp.get( "/admin/add", adminMiddleware, addPost );
-    // oApp.get( "/admin/edit/:file.json", editPost );
-    // oApp.post( "/admin/save", savePost );
-    // oApp.get( "/admin/delete/:file.json", deletePost );
+    // oApp.get( "/admin/edit/:file.json", adminMiddleware, editPost );
+    oApp.post( "/admin/save", adminMiddleware, savePost );
+    // oApp.get( "/admin/delete/:file.json", adminMiddleware, deletePost );
     // oApp.get( "/admin/exit", logout );
 };
